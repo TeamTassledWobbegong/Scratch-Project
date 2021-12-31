534e557eaa7ec5bff3162c0af3d9a3e6c5b74a34
Ronak Hirpara <ronak@Ronaks-MBP.fios-router.home>
2021-12-31 14:43:30 -0500

    added boilerplate

---
+++ client/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const root = document.getElementById('root');
+    const msg = document.createElement('div');
+
+    root.appendChild(msg);
+  })
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  entry: [
+    './client/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer:{
+    static:{
+        directory: path.join(__dirname),
+    },
+    proxy: {
+        '/api':'http://localhost:3000'
+    },
+    compress: true,
+    port: 8080,
+    hot:true,
+  
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './client/index.html')
+    }),
+    new MiniCssExtractPlugin()
+  ]
+};
+
+module.exports = config;
